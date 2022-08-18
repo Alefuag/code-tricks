045019707639bb2a1e5d30b736f2ad0232b21dbc
Alejandro Furió <65417428+Alefuag@users.noreply.github.com>
2022-08-19 02:14:15 +0900

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,9 @@
 
 ## Retrieve an object from its memory addres
-
 import ctypes
 a = "hello world"
 print(ctypes.cast(id(a), ctypes.py_object).value)
 
+
+## Learn module and package importing (including __init__.py)
 
